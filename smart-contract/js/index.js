131b8ad2a58ec2ee82dd94163727a9b216a375f5
ayoubioussoufa <ayoubioussoufa@gmail.com>
2024-05-09 17:29:34 +0100

    slowly but surely

--- smart-contract/js/index.js
+++ smart-contract/js/index.js
@@ -1,35 +1,115 @@
-// import { abi, contractAddress } from "./constants.js"
+import { abi, contractAddress } from "./constants.js";
 import Web3 from 'web3';
 
-// connectButton.onclick = connect
+// import ganache from 'ganache';
+// const Web3 = require("web3");
+// const ganache = require("ganache");
+
+let connectButton = document.getElementById('btn');
+let createTournament = document.getElementById('createTournament');
+let register = document.getElementById('register');
+
+
+const web3 = new Web3("https://eth-sepolia.g.alchemy.com/v2/oox3rnLcR1MwEjduwOmOsXVGb2pawXso");
+const contract = new web3.eth.Contract(abi, contractAddress);
+
+connectButton.onclick = connect
+createTournament.onclick = createTournaments
+register.onclick = register
 
 // Create a new Web3 instance with the provider of your choice
 // const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
 //Replace 'https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID' with the URL of your preferred Ethereum node provider, such as Infura, Alchemy, or your own local no
 
-// async function connect() {
-//     const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:8545"); // connected to our blockchain
-//     const web3 = new Web3(provider);
-//     console.log(web3);
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        await ethereum.request({ method: "eth_requestAccounts" })
+      } catch (error) {
+        console.log(error)
+      }
+      connectButton.innerHTML = "Connected"
+      const accounts = await ethereum.request({ method: "eth_accounts" })
+      console.log(accounts)
+    } else {
+      connectButton.innerHTML = "Please install MetaMask"
+    }
+}
 
-//     // to fetch the account addresses
-//     const accounts = await web3.eth.getAccounts();
-//     console.log(accounts[0]);
+async function createTournaments() {
+    await web3.eth.methods.createTournament().send(accounts[0].address)
+    .then(data => {
+        console.log("Tournament Created");
+        createTournament.innerHTML = "Tournament Created";
+    })
+    .catch(error => {
+        console.log("Tournament was not created");
+    });
+}
 
-//     // to fetch the account address balance
-//     const balance = await web3.eth.getBalance(accounts[0]);
-//     console.log(balance);
-
-//     // to convert our balance to ether
-//     const balanceEther = web3.utils.fromWei(balance, "ether");
-//     console.log(balanceEther);
-
-
+// async function register() {
+    
 // }
 
 // connect();
 
-async function fromSmartContract() {
-    const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:8545"); // connected to our blockchain
-    const web3 = new Web3(provider);
-}
+// async function fromSmartContract() {
+//     // const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:8545"); // connected to our blockchain
+//     // const web3 = new Web3(provider);
+//     // or
+//     const web3 = new Web3("https://eth-sepolia.g.alchemy.com/v2/oox3rnLcR1MwEjduwOmOsXVGb2pawXso");
+//     const contract = new web3.eth.Contract(abi, contractAddress);
+//     const account = web3.eth.accounts.wallet.add('0x08fdb79d0797a2f3be23e65c04b7ea373873132a8b0c1952298ebd103dafa656');
+//     console.log(account[0].address);
+//     console.log(account[0].privateKey);
+//     const walletAddress = account[0].address;
+//     web3.eth.getBalance(walletAddress)
+//         .then(balance => {
+//           console.log('Wallet Address:', walletAddress);
+//           console.log('Balance:', web3.utils.fromWei(balance, 'ether'), 'ETH');
+//         })
+//         .catch(error => {
+//           console.error('Error:', error);
+//         });
+    // await contract.methods.createTournament().send({ from: '0xc70Cd98fcB36cCfE70a571bB9A646FBF479e177A' })
+    //     .on('transactionHash', function(hash){
+    //       console.log('Transaction Hash:', hash);
+    //     })
+    //     .on('receipt', function(receipt){
+    //       console.log('Receipt:', receipt);
+    //     })
+    //     .on('error', function(error){
+    //       console.error('Error calling createTournament:', error);
+    //     });
+    // console.log(contract);
+//     contract.methods.tournamentId().call().then(console.log);
+//     const tournId = await contract.methods.tournamentId().call();
+//     console.log("Tournament Id: ", tournId);
+//     // await contract.methods.registerForTournament('0xF140580EABcDc1599c3200b4D6Bf85cB43fBaD3E').send({from: '0xc70Cd98fcB36cCfE70a571bB9A646FBF479e177A'})
+//     //     .on('transactionHash', function(hash){
+//     //           console.log('Transaction Hash:', hash);
+//     //         })
+//     //         .on('receipt', function(receipt){
+//     //           console.log('Receipt:', receipt);
+//     //         })
+//     //         .on('error', function(error){
+//     //           console.error('Error calling createTournament:', error);
+//     //         });
+//     const playersRegistred = await contract.methods.getNumberOfPlayersRegistred(tournId).call();
+//     console.log("Players Registred: ", playersRegistred);
+//     // const sub = contract.events.methods.createTournament()
+//     // sub.on("data", console.log);
+//     // contract.methods.createTournament().call((error, result) => {
+//     //     if (error) {
+//     //         console.error('Error calling myFunction:', error);
+//     //       } else {
+//     //         console.log('Result of myFunction:', result);
+//     //       }
+//     // });
+//     // console.log(data);
+//     // console.log(contract.methods.tournamentId());
+//     // console.log(tournamentId);
+//     //create random wallet with 1 account
+// }
+
+// fromSmartContract()
